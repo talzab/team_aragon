263147c33413c3b557b3ca48be031004c2bbcb7c
talzab <144385909+talzab@users.noreply.github.com>
2023-11-16 11:39:00 -0500

    Add files via upload

--- load_hss.py
+++ load_hss.py
@@ -1,84 +1,91 @@
 import pandas as pd
+import numpy as np
 import psycopg
 
-def check_duplicate_id(cursor, table_name, id_column, value):
-    # Check if the ID already exists in the specified table
-    cursor.execute(f"SELECT 1 FROM {table_name} WHERE {id_column} = %s", (value,))
-    return cursor.fetchone() is not None
+def check_duplicate_id(curr, table_name, column_name, value, column_name2 = None, value2 = None):
+    if column_name2 is None:
+        query = f"SELECT COUNT(*) FROM {table_name} WHERE {column_name} = %s"
+        curr.execute(query, (value,))
+    else:
+        query = f"SELECT COUNT(*) FROM {table_name} WHERE {column_name} = %s AND {column_name2} = %s"
+        curr.execute(query, (value, value2))
 
-def load_hhs_data(csv_file, connection):
-    # Load CSV file into a pandas DataFrame
-    df = pd.read_csv(csv_file)
-    print("File read in...")
+    count = curr.fetchone()[0]
+    return count > 0
+
+
+def load_hhs_data(csv_file, conn):
+    try:
+        # Load CSV file into a pandas DataFrame
+        df = pd.read_csv(csv_file)
+    except Exception as e:
+        print(f"Error reading CSV file: {e}")
 
     # Do necessary processing on the DataFrame (e.g., handle -999 values, parse dates)
     df.replace(-999999, None, inplace=True) 
-    df.columns = df.columns.str.lower().str.replace(" ", "_")
-    print("Preprocessing complete...")
+    df.replace({np.nan: None}, inplace=True)
 
-    # Parse date columns if needed
-    # df['collection_week'] = pd.to_datetime(df['collection_week'])
+    df = df.astype(float, errors='ignore')
+    df['collection_week'] = pd.to_datetime(df['collection_week'], format = '%Y-%m-%d').dt.date
 
+    
     # Create a cursor and open a transaction
-    cursor = connection.cursor()
-
-    cursor.execute('''
-                   INSERT INTO Hospitals (hospital_pk, hospital_name, collection_week)
-                    VALUES (%s, %s, %s)
-                ''', ('029182', 'Hospital A', '2022-03-23'))
-    print("inserted hospital")
+    curr = conn.cursor()
 
     try:
         # Insert data into the database
         for index, row in df.iterrows():
+            print(f"Processing row {index}")
+
             try:
-                print("in here...")
-
                 # Insert into Hospitals table
-                cursor.execute('''
-                    INSERT INTO Hospitals (hospital_pk, hospital_name, collection_week)
-                    VALUES (%s, %s, %s)
-                ''', (row['hospital_pk'], row['hospital_name'], row['collection_week']))
+                if check_duplicate_id(curr, 'Hospitals', 'hospital_pk', row['hospital_pk']):
+                    raise ValueError(f"Duplicate ID found in Hospitals table: {row['hospital_pk']}")
+                curr.execute(''' INSERT INTO Hospitals (hospital_pk, hospital_name) VALUES (%s, %s)''', 
+                         (row['hospital_pk'], row['hospital_name']))
+            except Exception as e:
+                print(f"Error inserting data into Hospitals for row {index}: {e}")
+                continue
 
+            try:
                 # Insert into HospitalLocations table
-                cursor.execute('''
-                    INSERT INTO HospitalLocations (hospital_fk, state, address, city, zip, fips_code, geocoded_hospital_address)
-                    VALUES (%s, %s, %s, %s, %s, %s, %s)
-                ''', (row['hospital_pk'], row['state'], row['address'], row['city'], row['zip'], row['fips_code'], row['geocoded_hospital_address']))
+                if check_duplicate_id(curr, 'HospitalLocations', 'hospital_fk', row['hospital_pk']):
+                    raise ValueError(f"Duplicate ID found in HospitalLocation table: {row['hospital_pk']}")
+                curr.execute(''' INSERT INTO HospitalLocations (hospital_fk, state, address, city, zip, fips_code, geocoded_hospital_address) VALUES (%s, %s, %s, %s, %s, %s, %s)''', 
+                         (row['hospital_pk'], row['state'], row['address'], row['city'], row['zip'], row['fips_code'], row['geocoded_hospital_address']))
+            except Exception as e:
+                print(f"Error inserting data into HospitalLocations for row {index}: {e}")
+                continue
 
+            try:
                 # Insert into HospitalBedInformation table
-                cursor.execute('''
-                    INSERT INTO HospitalBedInformation (hospital_fk, all_adult_hospital_beds_7_day_avg, all_pediatric_inpatient_beds_7_day_avg, 
+                if check_duplicate_id(curr, 'HospitalBedInformation', 'hospital_fk', row['hospital_pk'], 'collection_week', row['collection_week']):
+                    raise ValueError(f"Duplicate ID and Collection Date found in HospitalBedInformation table: {row['hospital_pk'], row['collection_date']}")
+                curr.execute(''' INSERT INTO HospitalBedInformation (hospital_fk, collection_week, all_adult_hospital_beds_7_day_avg, all_pediatric_inpatient_beds_7_day_avg, 
                         all_adult_hospital_inpatient_bed_occupied_7_day_coverage, all_pediatric_inpatient_bed_occupied_7_day_avg, 
                         total_icu_beds_7_day_avg, icu_beds_used_7_day_avg, inpatient_beds_used_covid_7_day_avg, 
                         staffed_icu_adult_patients_confirmed_covid_7_day_avg)
-                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
-                ''', (row['hospital_pk'], row['all_adult_hospital_beds_7_day_avg'], row['all_pediatric_inpatient_beds_7_day_avg'],
+                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)''', 
+                    (row['hospital_pk'], row['collection_week'], row['all_adult_hospital_beds_7_day_avg'], row['all_pediatric_inpatient_beds_7_day_avg'],
                     row['all_adult_hospital_inpatient_bed_occupied_7_day_coverage'], row['all_pediatric_inpatient_bed_occupied_7_day_avg'],
                     row['total_icu_beds_7_day_avg'], row['icu_beds_used_7_day_avg'], row['inpatient_beds_used_covid_7_day_avg'],
                     row['staffed_icu_adult_patients_confirmed_covid_7_day_avg']))
-
             except Exception as e:
-                print(f"Error inserting data for row {index}: {e}")
-                print("Row causing the issue:", row)
+                print(f"Error inserting data into HospitalBedInformation for row {index}: {e}")
+                continue
 
         # Commit the changes
-        connection.commit()
-
-        cursor.execute("SELECT * FROM Hospitals limit 10")
-        results = cursor.fetchall()
-        for row in results:
-            print(row)
-
+        conn.commit()
         print("Data loaded successfully.")
+
     except Exception as e:
         print(f"Error: {e}")
         # Rollback the transaction in case of an error
-        connection.rollback()
+        conn.rollback()
         raise ValueError("Data loading failed.") from e
     finally:
         # Close the cursor
-        cursor.close()
+        curr.close()
 
 if __name__ == "__main__":
     import sys
@@ -90,18 +97,16 @@ if __name__ == "__main__":
     csv_file = sys.argv[1]
 
     try:
-        # Connect to the PostgreSQL database
-        connection = psycopg.connect(
-            host="pinniped.postgres.database.azure.com",
-            dbname="talzaben",
-            user="talzaben",
-            password="klVgh!KCGA"
-        )
-
-        load_hhs_data(csv_file, connection)
+        with psycopg.connect(
+                host="pinniped.postgres.database.azure.com",
+                dbname="talzaben",
+                user="talzaben",
+                password="klVgh!KCGA"
+        ) as conn:
+            load_hhs_data(csv_file, conn)
     except ValueError as ve:
         print(ve)
     finally:
         # Close the database connection
-        if connection:
-            connection.close()
+        if conn:
+            conn.close()
